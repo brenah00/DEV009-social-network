79da0be606139eec5fc5fcac2ab32f3816dd91b8
Adriana-Salazar <adrianabsalazar@gmail.com>
2023-07-20 11:37:44 -0500

    constante route

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 // Este es el punto de entrada de tu aplicacion
 
-import login from './components/login';
+import login from "./components/login";
+import register from "./components/register";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
-root.append(login());
+const routes = [
+  { path: "/", component: login },
+  { path: "/register", component: register },
+];
+
+const defaultRoute = "/"; .
