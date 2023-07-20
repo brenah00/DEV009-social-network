f214446311f28f260ee49a2eefbe6c5ce66f9407
Brenda Aguilar <blenda916@gmail.com>
2023-07-20 16:02:28 -0600

    Campos de formulario de registro

--- src/components/register.js
+++ src/components/register.js
@@ -1,13 +1,27 @@
 function register(navigateTo) {
   const section = document.createElement("section");
   const title = document.createElement("h2");
-  title.textContent = "REGISTRO";
   const button = document.createElement('button');
+  const userName = document.createElement('input');
+  const userLastName = document.createElement('input');
+  const userEmail = document.createElement('input');
+  const userBirthDate = document.createElement('input');
+  const userPassword = document.createElement('input');
+
+  title.textContent = "REGISTRO";
+  userName.placeholder = 'Nombre';
+  userLastName.placeholder = 'Apellidos';
+  userEmail.placeholder = 'Correo electrónico';
+  userEmail.type = 'email';
+  userBirthDate.placeholder = 'Fecha de nacimiento';
+  userBirthDate.type = 'date';
+  userPassword.placeholder = 'Contraseña';
+  userPassword.type = 'password';
   button.textContent = 'Registrarse';
   button.addEventListener('click',()=>{
     navigateTo('/home');
   });
-  section.append(title,button);
+  section.append(title,userName,userLastName,userEmail,userBirthDate,userPassword,button);
   return section;
 }
 export default register;
