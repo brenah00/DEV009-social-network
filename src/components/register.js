81c2b134a35156f768b67574da89d2a8b5210827
Aivy Aguirre <aivy3094@gmail.com>
2023-07-19 19:00:35 -0500

    archivo register

---
+++ src/components/register.js
@@ -0,0 +1,9 @@
+function register() {
+    const section = document.createElement("section");
+    const title = document.createElement("h2");
+    title.textContent = "REGISTRO";
+    section.append(title);
+    return section;
+  }
+  export default register;
+  
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 // Este es el punto de entrada de tu aplicacion
 
-import login from './components/login';
+import login from './components/login.js';
+import register from './components/register.js';
 
 const root = document.getElementById('root');
 
 root.append(login());
+
+root.append(register());
