8ebe1c3cb7e085b5db492c5c27a064f65a26efc5
Adriana-Salazar <adrianabsalazar@gmail.com>
2023-08-01 11:58:19 -0500

    Maquetacion area new post

--- src/components/home.js
+++ src/components/home.js
@@ -8,18 +8,23 @@ async function showName(user) {
   user.innerHTML = await showUserName(await getEmail());
 }
 function home(navigateTo) {
+  const newPost = document.createElement('textarea');  
   const section = document.createElement('section');
+  section.id = 'contentHome'
   const title = document.createElement('h2');
-  const user = document.createElement('p');
+  const user = document.createElement('p');  
   title.textContent = 'HOME';
   const button = document.createElement('button');
   button.textContent = 'Cerrar sesión';
+  newPost.placeholder = 'Que estás escuchando?';
+  newPost.id = 'newPost';
   showName(user);
   button.addEventListener('click', () => {
     navigateTo('/');
     logoutUser();
+
   });
-  section.append(title, user, button);
+  section.append(title, user, newPost, button);
   return section;
 }
 export default home;
