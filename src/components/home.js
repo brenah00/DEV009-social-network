b18fc2493c0031bda2bdf676996a7bc58b6d833e
Adriana-Salazar <adrianabsalazar@gmail.com>
2023-08-03 19:20:16 -0500

    se valida que el campo de publicacion no esté vacio

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,5 @@
-import {
-  getEmail,
-  logoutUser,
-} from '../lib/authentication.js';
-import {
-  showUserName,
-  showPost,
-  newPost,
-} from '../lib/firestore.js';
+import { getEmail, logoutUser } from "../lib/authentication.js";
+import { showUserName, showPost, newPost } from "../lib/firestore.js";
 
 async function showName(user) {
   user.innerHTML = await showUserName(await getEmail());
@@ -21,15 +14,15 @@ async function showPosts(sectionPost) {
     return dateB - dateA;
   });
   // Limpiamos la sección de posts antes de agregar los posts ordenados
-  sectionPost.innerHTML = '';
+  sectionPost.innerHTML = "";
   sortedPosts.forEach((post) => {
-    const contentPost = document.createElement('div');
-    contentPost.className = 'post-box';
-    const textContentPost = document.createElement('div');
-    textContentPost.className = 'text-box';
-    const creator = document.createElement('h3');
-    const postContent = document.createElement('p');
-    const dateInformation = document.createElement('p');
+    const contentPost = document.createElement("div");
+    contentPost.className = "post-box";
+    const textContentPost = document.createElement("div");
+    textContentPost.className = "text-box";
+    const creator = document.createElement("h3");
+    const postContent = document.createElement("p");
+    const dateInformation = document.createElement("p");
     dateInformation.textContent = post.date;
 
     // Obtenemos el nombre del usuario que publicó el post
@@ -45,30 +38,40 @@ async function showPosts(sectionPost) {
 }
 
 function home(navigateTo) {
-  const textPost = document.createElement('textarea');
-  const section = document.createElement('section');
-  section.id = 'contentHome';
-  const title = document.createElement('h2');
-  const user = document.createElement('p');
-  title.textContent = 'HOME';
-  const buttonPublish = document.createElement('button');
-  buttonPublish.textContent = 'Publicar';
-  const button = document.createElement('button');
-  button.textContent = 'Cerrar sesión';
-  textPost.placeholder = 'Que estás escuchando?';
-  const sectionPost = document.createElement('section');
-  sectionPost.className = 'post-section';
-  textPost.id = 'newPost';
+  const textPost = document.createElement("textarea");
+  const section = document.createElement("section");
+  section.id = "contentHome";
+  const title = document.createElement("h2");
+  const user = document.createElement("p");
+  title.textContent = "HOME";
+  const buttonPublish = document.createElement("button");
+  buttonPublish.textContent = "Publicar";
+  const messagePublish = document.createElement('p'); 
+  const button = document.createElement("button");
+  button.textContent = "Cerrar sesión";
+  textPost.placeholder = "Que estás escuchando?";
+  const sectionPost = document.createElement("section");
+  sectionPost.className = "post-section";
+  textPost.id = "newPost";
   showName(user);
-  button.addEventListener('click', () => {
-    navigateTo('/');
+  button.addEventListener("click", () => {
+    navigateTo("/");
     logoutUser();
   });
+
   buttonPublish.addEventListener('click', async () => {
-    await newPost(await getEmail(), textPost.value);
+    const postContent = textPost.value.trim(); // Obtener el contenido del campo de publicación y eliminar espacios en blanco al inicio y al final
+    if (postContent === '') {
+      messagePublish.textContent = 'El campo de publicación no puede estar vacío';
+      return;
+    }
+    // Si el campo no está vacío, ocultar el mensaje de error (si se mostró previamente) y proceder con la publicación
+    messagePublish.textContent = '';
+    await newPost(await getEmail(), postContent);
   });
+
   showPosts(sectionPost);
-  section.append(title, user, textPost, buttonPublish, sectionPost, button);
+  section.append(title, user, textPost, messagePublish, buttonPublish, sectionPost, button);
   return section;
 }
 export default home;
